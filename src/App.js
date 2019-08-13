587f1ac0cafb9d627f57bb886f96352a5773f7fa
mustran <ademimusli000@gmail.com>
2019-08-13 22:38:49 +0200

    collection preview + collection item added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => {
     return <h1>Hats Page</h1>;
@@ -10,6 +11,7 @@ const HatsPage = () => {
 function App() {
     return (
         <div>
+            <Route exact path="/shop" component={ShopPage} />
             <Route exact path="/" component={HomePage} />
             <Route path="/shop/hats" component={HatsPage} />
         </div>
