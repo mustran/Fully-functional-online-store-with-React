90762b10b7b9c865b023ffd623d030141d978a58
mustran <ademimusli000@gmail.com>
2019-08-22 21:37:59 +0200

    Button added to cart elements in shop page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./component-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
-const COllectionItem = ({ name, price, imageUrl }) => {
+const CollectionItem = ({ name, price, imageUrl }) => {
     return (
         <div className="collection-item">
             <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,8 +10,9 @@ const COllectionItem = ({ name, price, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton inverted>Add to cart</CustomButton>
         </div>
     );
 };
 
-export default COllectionItem;
+export default CollectionItem;
